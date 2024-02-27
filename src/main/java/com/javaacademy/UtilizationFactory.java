2bc09bb75aceb69f85089cc76f0c810b24c821e3
KlimenkoAV <aklimenko063@gmail.com>
2024-02-27 20:26:18 +0400

    tsp-0108 Добавил вызов builder в методы генерации ошибок

--- src/main/java/com/javaacademy/UtilizationFactory.java
+++ src/main/java/com/javaacademy/UtilizationFactory.java
@@ -15,7 +15,9 @@ public class UtilizationFactory {
     @SneakyThrows
     private Bottle refactorGlassGarbage(Garbage garbage) {
         if (garbage.getGarbageType() != GarbageType.GLASS) {
-            throw new GarbageNotRefactorableException("Мусор не состоит полностью из стекла!");
+            throw GarbageNotRefactorableException.builder()
+                    .message("Мусор не состоит полностью из стекла!")
+                    .build();
         }
         return new Bottle(BOTTLE_SIZE, garbage.getCityFrom());
     }
@@ -23,7 +25,9 @@ public class UtilizationFactory {
     @SneakyThrows
     private Cartoon refactorPaperGarbage(Garbage garbage) {
         if (garbage.getGarbageType() != GarbageType.PAPER) {
-            throw new GarbageNotRefactorableException("Мусор не состоит полностью из бумаги!");
+            throw GarbageNotRefactorableException.builder()
+                    .message("Мусор не состоит полностью из бумаги!")
+                    .build();
         }
         return new Cartoon(garbage.getWeight() / 2);
     }
@@ -32,9 +36,18 @@ public class UtilizationFactory {
     public void refactorGarbage(Garbage[] garbageArray, BufferedWriter journal) {
         for (Garbage garbage : garbageArray) {
             switch (garbage.getGarbageType()) {
-                case GLASS -> journal.write(JournalRecord.builder().bottle(UtilizationFactory.refactorGlassGarbage(garbage)).build().toString());
-                case PAPER -> journal.write(JournalRecord.builder().cartoon(UtilizationFactory.refactorPaperGarbage(garbage)).build().toString());
-                default -> journal.write(JournalRecord.builder().garbageWeight(garbage.getWeight()).build().toString());
+                case GLASS -> journal.write(JournalRecord.builder()
+                        .bottle(UtilizationFactory.refactorGlassGarbage(garbage))
+                        .build()
+                        .toString());
+                case PAPER -> journal.write(JournalRecord.builder()
+                        .cartoon(UtilizationFactory.refactorPaperGarbage(garbage))
+                        .build()
+                        .toString());
+                default -> journal.write(JournalRecord.builder()
+                        .garbageWeight(garbage.getWeight())
+                        .build()
+                        .toString());
             }
         }
     }
